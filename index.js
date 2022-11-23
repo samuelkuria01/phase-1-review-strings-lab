cc240fce4c41896d84222549a08de7420217ceed
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2022-11-23 22:01:00 +0300

    nice try

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser.at(0,1)}!`;
